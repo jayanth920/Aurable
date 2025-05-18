ccc898f761d5b763a1bc31fdd744da4207c9332d
jayanth920 <113549918+jayanth920@users.noreply.github.com>
2025-05-18 13:52:07 -0500

    Homepage init: components/custom/AppSideBar.jsx

---
+++ components/custom/AppSideBar.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarGroup,
+  SidebarHeader,
+} from '@/components/ui/sidebar';
+import Image from 'next/image';
+import { Button } from '../ui/button';
+import { MessageCircleCodeIcon } from 'lucide-react';
+import WorkspaceHistory from './WorkspaceHistory';
+import SideBarFooter from './SideBarFooter';
+import Link from 'next/link';
+
+function AppSideBar() {
+  return (
+    
+      <Sidebar>
+        <SidebarHeader className="p-5">
+            {/* <Image src={'/logo.png'} alt="logo" width={30} height={30} /> */}
+            <Link href={'/'}><Button className="mt-5"><MessageCircleCodeIcon /> Start New Chat</Button></Link>
+        </SidebarHeader>
+        <SidebarContent className="">
+          <SidebarGroup>
+            <WorkspaceHistory />
+          </SidebarGroup>
+          {/* <SidebarGroup /> */}
+        </SidebarContent>
+        <SidebarFooter>
+            <SideBarFooter></SideBarFooter>
+        </SidebarFooter>
+      </Sidebar>
+    
+  );
+}
+
+export default AppSideBar;
