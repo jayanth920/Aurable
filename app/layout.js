0606dcb1dc0dce20813a9297cd93b0b6ee64b647
jayanth920 <113549918+jayanth920@users.noreply.github.com>
2025-05-19 16:19:27 -0500

    logout,signin button, repeated user fixed: app/layout.js

--- app/layout.js
+++ app/layout.js
@@ -13,7 +13,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="en" suppressHydrationWarning className="light">
       <body>
         <ConvexClientProvider>
           <Provider>{children}</Provider>
