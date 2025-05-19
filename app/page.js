2d8a409758a655d73088a8476b636c1a06ad8efe
jayanth920 <113549918+jayanth920@users.noreply.github.com>
2025-05-19 16:19:27 -0500

    logout,signin button, repeated user fixed: app/page.js

--- app/page.js
+++ app/page.js
@@ -1,10 +1,11 @@
 import Hero from "@/components/custom/Hero";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
-
+import { Title } from "@/components/custom/Title";
 export default function Home() {
   return (
-    <div className="mx-auto max-h-1vh overflow-hidden">
+    <div className="mx-auto max-h-1vh overflow-hidden flex flex-col items-center justify-center">
+      <Title/>
       <Hero /> 
     </div>
   );
