62fde6083eff4c28d60812300b0527e200e12455
jayanth920 <113549918+jayanth920@users.noreply.github.com>
2025-05-20 03:02:01 -0500

    chat part: components/custom/WorkspaceHistory.jsx

--- components/custom/WorkspaceHistory.jsx
+++ components/custom/WorkspaceHistory.jsx
@@ -1,10 +1,10 @@
-'use client';
-import { UserDetailContext } from '@/context/UserDetailContext';
-import { api } from '@/convex/_generated/api';
-import { useConvex } from 'convex/react';
-import Link from 'next/link';
-import React, { useContext, useEffect, useState } from 'react';
-import { useSidebar } from '../ui/sidebar';
+"use client";
+import { UserDetailContext } from "@/context/UserDetailContext";
+import { api } from "@/convex/_generated/api";
+import { useConvex } from "convex/react";
+import Link from "next/link";
+import React, { useContext, useEffect, useState } from "react";
+import { useSidebar } from "../ui/sidebar";
 
 function WorkspaceHistory() {
   const convex = useConvex();
@@ -23,17 +23,21 @@ function WorkspaceHistory() {
     });
     setWorkSpaceList(result);
   };
-  
 
   return (
     <div>
-      <h2 className="font-medium text-lg">Your Chats</h2>
+      <h2 className="font-medium text-lg ml-3">Your Chats</h2>
       <div>
         {workspaceList &&
           workspaceList?.map((workspace, index) => (
-            <Link key={index} href={'/workspace/' + workspace?._id}>
-              <h2 onClick={toggleSidebar} className="text-sm text-gray-400 mt-2 font-light hover:text-white cursor-pointer">
-                {workspace?.messages[0]?.content}
+            <Link key={index} href={"/workspace/" + workspace?._id}>
+              <h2
+                onClick={toggleSidebar}
+                className="text-sm text-gray-400 mt-2 ml-6 font-light hover:text-white cursor-pointer"
+              >
+                {workspace?.messages[0]?.content.length > 20
+                  ? workspace?.messages[0]?.content.slice(0, 30) + "..."
+                  : workspace?.messages[0]?.content}
               </h2>
             </Link>
           ))}
