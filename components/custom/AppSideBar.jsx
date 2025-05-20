cb9805ddba4409e028c07d58529ba580bd403089
jayanth920 <113549918+jayanth920@users.noreply.github.com>
2025-05-20 03:02:01 -0500

    chat part: components/custom/AppSideBar.jsx

--- components/custom/AppSideBar.jsx
+++ components/custom/AppSideBar.jsx
@@ -12,16 +12,17 @@ import { MessageCircleCodeIcon } from "lucide-react";
 import WorkspaceHistory from "./WorkspaceHistory";
 import SideBarFooter from "./SideBarFooter";
 import Link from "next/link";
+import { ButtonCta } from "../ui/button-shiny";
 
 function AppSideBar() {
   return (
     <Sidebar>
-      <SidebarHeader className="p-5">
+      <SidebarHeader className="flex justify-between items-center">
         {/* <Image src={'/logo.png'} alt="logo" width={30} height={30} /> */}
         <Link href={"/"}>
-          <Button className="mt-5">
-            <MessageCircleCodeIcon /> Start New Chat
-          </Button>
+          <ButtonCta className="mt-5 w-fit h-10 text-base font-normal hover:shadow-[0_0_1px_2px_rgba(168,85,247,0.9)] transition-all" label="Start New Chat">
+            <MessageCircleCodeIcon />
+          </ButtonCta>
         </Link>
       </SidebarHeader>
       <SidebarContent className="">
