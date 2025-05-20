43d4ec03bae8db17edbed2a2c9fb01ce65e2224c
jayanth920 <113549918+jayanth920@users.noreply.github.com>
2025-05-20 03:02:01 -0500

    chat part: components/custom/Title.jsx

--- components/custom/Title.jsx
+++ components/custom/Title.jsx
@@ -1,18 +1,22 @@
-import React from 'react';
+import React from "react";
+import { GradientText } from "../ui/gradient-text";
+import { SparklesText } from "@/components/ui/sparkles-text";
 
 export const Title = () => {
-  return (
-    <div
-      className="w-full flex justify-center items-baseline"
-      style={{
-        fontFamily: 'KaiseiOpti',
-        textShadow: "0 2px 2px rgba(0, 0, 0, 0.8)"
-      }}
-    >
-      <p className="leading-none text-amber-300 subpixel-antialiased font-medium">
-        <span className="text-[5rem] align-baseline tracking-widest">A</span>
-        <span className="text-[3rem] align-baseline tracking-widest">URABLE</span>
-      </p>
-    </div>
+  return(
+  <div
+    className="w-full flex justify-center items-baseline leading-none"
+    style={{
+      fontFamily: "Gloock",
+      textShadow: "0 2px 2px rgba(0, 0, 0, 0.8)",
+    }}
+  >
+    <p className="leading-none text-amber-300 subpixel-antialiased font-medium tracking-widest">
+      <span className="text-[5rem] align-baseline tracking-widest">A</span>
+      <span className="text-[3rem] align-baseline tracking-widest font-bold">
+        URABLE
+      </span>
+    </p>
+  </div>
   );
 };
